72236833a6beee1748e1262908ecfad681cd98d4
Mik-pe <mikael.pettersson201@gmail.com>
2019-12-26 21:53:37 +0100

    Initial commit
    A first working commit that handles some very naïve texture uploads

---
+++ src/main.rs
@@ -0,0 +1,300 @@
+use gl;
+
+use glutin::{ContextBuilder, EventsLoop, WindowBuilder};
+use std::ffi::CStr;
+use std::time::{Duration, Instant};
+static VS_SHADER_SRC: &'static [u8] = b"
+#version 450
+
+out vec2 tex_coords;
+void main()
+{
+    tex_coords = vec2(0.0, 0.0);
+    if(gl_VertexID == 0){
+        tex_coords = vec2(0.0, 0.0);
+        gl_Position = vec4(-0.5, -0.5, 0.0, 1.0);   
+    }
+    else if(gl_VertexID == 1){
+        tex_coords = vec2(0.5, 1.0);
+        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);   
+    }
+    else if(gl_VertexID == 2){
+        tex_coords = vec2(1.0, 0.0);
+        gl_Position = vec4(0.5, -0.5, 0.0, 1.0);   
+    }else{
+        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);   
+    }
+}\0";
+
+static FS_SHADER_SRC: &'static [u8] = b"
+#version 450
+layout(binding=0) uniform sampler2D tex_sampler;
+
+in vec2 tex_coords;
+
+out vec4 out_col;
+
+void main()
+{
+    vec4 color = texture(tex_sampler, tex_coords);
+    out_col = vec4(color.rgb, 1.0);
+    // out_col = vec4(tex_coords.r,tex_coords.g, 0.0, 1.0);
+}\0";
+
+macro_rules! glchk {
+    ($($s:stmt;)*) => {
+        $(
+            $s;
+            if cfg!(debug_assertions) {
+                let err = gl::GetError();
+                if err != gl::NO_ERROR {
+                    let err_str = match err {
+                        gl::INVALID_ENUM => "GL_INVALID_ENUM",
+                        gl::INVALID_VALUE => "GL_INVALID_VALUE",
+                        gl::INVALID_OPERATION => "GL_INVALID_OPERATION",
+                        gl::INVALID_FRAMEBUFFER_OPERATION => "GL_INVALID_FRAMEBUFFER_OPERATION",
+                        gl::OUT_OF_MEMORY => "GL_OUT_OF_MEMORY",
+                        gl::STACK_UNDERFLOW => "GL_STACK_UNDERFLOW",
+                        gl::STACK_OVERFLOW => "GL_STACK_OVERFLOW",
+                        _ => "unknown error"
+                    };
+                    println!("{}:{} - {} caused {}",
+                             file!(),
+                             line!(),
+                             stringify!($s),
+                             err_str);
+                }
+            }
+        )*
+    }
+}
+
+enum Message {
+    Upload,
+    Exit,
+}
+
+enum TextureUploaded {
+    Request(u32),
+    Acknowledgement(u32),
+}
+
+fn make_shader(shader_type: gl::types::GLenum, shader_src: &[u8]) -> u32 {
+    unsafe {
+        let shader_id = gl::CreateShader(shader_type);
+        //src to CStr
+        let src_cstr = CStr::from_bytes_with_nul(shader_src).unwrap();
+        let shader_len = src_cstr.to_bytes().len() as i32;
+        gl::ShaderSource(
+            shader_id,
+            1,
+            &src_cstr.as_ptr() as *const *const _,
+            &shader_len as *const _,
+        );
+        gl::CompileShader(shader_id);
+        let mut success = 0;
+        gl::GetShaderiv(shader_id, gl::COMPILE_STATUS, &mut success);
+        if success <= 0 {
+            let mut info_log = [0i8; 512];
+            let mut placeholder = 0;
+            gl::GetShaderInfoLog(shader_id, 512, &mut placeholder, info_log.as_mut_ptr());
+            let cstrinfo = CStr::from_ptr(info_log.as_ptr());
+            println!("Shader compilation error: \n{}", cstrinfo.to_str().unwrap());
+        };
+        shader_id
+    }
+}
+
+fn link_program(vs_shader: u32, fs_shader: u32) -> u32 {
+    unsafe {
+        let program = gl::CreateProgram();
+        gl::AttachShader(program, vs_shader);
+        gl::AttachShader(program, fs_shader);
+        gl::LinkProgram(program);
+        let mut success = 0;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut success);
+        let mut placeholder = 0;
+        if success <= 0 {
+            let mut info_log = [0i8; 512];
+            gl::GetProgramInfoLog(program, 512, &mut placeholder, info_log.as_mut_ptr());
+            let cstrinfo = CStr::from_ptr(info_log.as_ptr());
+            println!("Program link error: \n{}", cstrinfo.to_str().unwrap());
+        }
+        program
+    }
+}
+
+fn main() {
+    let (sender, receiver) = std::sync::mpsc::channel();
+    let (tex_sender, tex_receiver) = std::sync::mpsc::channel();
+
+    let mut events_loop = EventsLoop::new();
+    let window = WindowBuilder::new();
+    let gl_context = ContextBuilder::new()
+        .with_vsync(true)
+        .build_windowed(window, &events_loop)
+        .unwrap();
+
+    let gl_window = unsafe { gl_context.make_current() }.unwrap();
+
+    gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
+
+    let upload_events_loop = EventsLoop::new();
+    let upload_context = ContextBuilder::new()
+        .with_shared_lists(&gl_window)
+        .build_headless(
+            &upload_events_loop,
+            glutin::dpi::PhysicalSize::new(1024.0, 1024.0),
+        )
+        .unwrap();
+    let upload_thread = std::thread::spawn(move || {
+        let _upload_context = unsafe { upload_context.make_current() }.unwrap();
+        //TODO: Implement texture upload and sync channel
+        let mut current_green = 0u8;
+        loop {
+            let message = receiver.recv().unwrap();
+            match message {
+                Message::Upload => unsafe {
+                    // let start = Instant::now();
+                    let mut tex = 0u32;
+                    gl::CreateTextures(gl::TEXTURE_2D, 1, &mut tex);
+                    let num_mipmaps = 10;
+                    gl::TextureStorage2D(tex, num_mipmaps, gl::RGBA8, 1024, 1024);
+                    let mut img: image::RgbaImage = image::ImageBuffer::new(1024, 1024);
+                    for pixel in img.pixels_mut() {
+                        *pixel = image::Rgba([255, current_green, 255, 255]);
+                    }
+                    current_green = current_green.wrapping_add(10);
+                    gl::TextureSubImage2D(
+                        tex,
+                        0, // level
+                        0, // xoffset
+                        0, // yoffset
+                        1024,
+                        1024,
+                        gl::RGBA,
+                        gl::UNSIGNED_BYTE,
+                        img.into_raw().as_ptr() as *const _,
+                    );
+                    gl::GenerateTextureMipmap(tex);
+                    //This glFinish ensures all previously recorded calls are realized by the server
+                    //which takes around 2 frames
+                    gl::Finish();
+                    // let end = start.elapsed().as_micros() as f64 / 1000.0;
+                    // println!("Upload thread created texture ID {}, took {} ms", tex, end);
+                    tex_sender
+                        .send(TextureUploaded::Acknowledgement(tex))
+                        .expect("Could not send Texture Ack");
+                },
+                Message::Exit => {
+                    break;
+                }
+            }
+        }
+        println!("Exiting upload thread!");
+    });
+
+    let mut tex_list = vec![];
+    let mut running = true;
+    let mut r = 0.3;
+    let mut g = 0.5;
+    let mut b = 0.3;
+    let mut highest_frametime = 0.0;
+    let vs_shader = make_shader(gl::VERTEX_SHADER, VS_SHADER_SRC);
+    let fs_shader = make_shader(gl::FRAGMENT_SHADER, FS_SHADER_SRC);
+    let program = link_program(vs_shader, fs_shader);
+    while running {
+        let start = Instant::now();
+        events_loop.poll_events(|event| {
+            use glutin::{Event, WindowEvent};
+            if let Event::WindowEvent { event, .. } = event {
+                match event {
+                    WindowEvent::CloseRequested => {
+                        running = false;
+                    }
+                    WindowEvent::KeyboardInput {
+                        device_id: _,
+                        input,
+                    } => match input.virtual_keycode {
+                        Some(keycode) => match keycode {
+                            glutin::VirtualKeyCode::Escape => {
+                                running = false;
+                            }
+                            glutin::VirtualKeyCode::Space => {
+                                sender
+                                    .send(Message::Upload)
+                                    .expect("Could not send Upload message");
+                            }
+                            glutin::VirtualKeyCode::B => {
+                                let mut tex = 0u32;
+                                unsafe {
+                                    glchk!(gl::CreateTextures(gl::TEXTURE_2D, 1, &mut tex););
+                                    let num_mipmaps = 10;
+                                    gl::TextureStorage2D(tex, num_mipmaps, gl::RGBA8, 1024, 1024);
+                                    let img: image::RgbaImage = image::ImageBuffer::new(1024, 1024);
+                                    let mut img: image::RgbaImage =
+                                        image::ImageBuffer::new(1024, 1024);
+                                    for pixel in img.pixels_mut() {
+                                        *pixel = image::Rgba([255, 128, 255, 255]);
+                                    }
+                                    gl::TextureSubImage2D(
+                                        tex,
+                                        0, // level
+                                        0, // xoffset
+                                        0, // yoffset
+                                        1024,
+                                        1024,
+                                        gl::RGBA,
+                                        gl::UNSIGNED_BYTE,
+                                        img.as_ptr() as *const _,
+                                    );
+                                    gl::GenerateTextureMipmap(tex);
+                                }
+                                println!("Creating texture from main thread, got ID {}", tex);
+                                tex_list.push(tex);
+                            }
+                            _ => {}
+                        },
+                        None => {}
+                    },
+                    _ => {}
+                }
+            }
+        });
+        let tex_result = tex_receiver.try_recv();
+        match tex_result {
+            Ok(TextureUploaded::Acknowledgement(result)) => {
+                tex_list.push(result);
+                unsafe {
+                    gl::BindTextureUnit(0, result);
+                }
+                // println!("Received texture, now got {} textures", tex_list.len());
+            }
+            _ => {}
+        }
+        unsafe {
+            gl::UseProgram(program);
+            // Clear the screen to black
+            gl::ClearColor(r, g, b, 1.0);
+            // r = (r + 0.01) % 1.0;
+            // g = (g + 0.01) % 1.0;
+            // b = (b + 0.01) % 1.0;
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            glchk!(gl::DrawArrays(gl::TRIANGLES, 0, 3););
+        }
+        gl_window
+            .window()
+            .set_title(format!("Got {} textures", tex_list.len()).as_str());
+        gl_window.swap_buffers().unwrap();
+        let end = start.elapsed().as_micros() as f64 / 1000.0;
+        highest_frametime = f64::max(highest_frametime, end);
+    }
+    sender
+        .send(Message::Exit)
+        .expect("Could not send Exit message!");
+    upload_thread.join().expect("Could not join threads!");
+    println!(
+        "All threads joined and finished, highest time was {}",
+        highest_frametime
+    );
+}
