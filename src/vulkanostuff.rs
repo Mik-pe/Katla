4a2d92a7706fe47cdd54069d0c1fa1418623d1ee
Mikael Pettersson <mikael.pettersson@liu.se>
2020-04-16 18:12:53 +0200

    Point my triangle upwards

--- src/vulkanostuff.rs
+++ src/vulkanostuff.rs
@@ -212,15 +212,15 @@ impl VulkanoCtx {
                 false,
                 [
                     vertextypes::VertexNormal {
-                        position: [-0.5, 0.5, 0.0],
+                        position: [-0.5, -0.5, 0.0],
                         normal: [1.0, 0.0, 0.0],
                     },
                     vertextypes::VertexNormal {
-                        position: [0.0, -0.5, 0.0],
+                        position: [0.0, 0.5, 0.0],
                         normal: [0.0, 1.0, 0.0],
                     },
                     vertextypes::VertexNormal {
-                        position: [0.5, 0.5, 0.0],
+                        position: [0.5, -0.5, 0.0],
                         normal: [0.0, 0.0, 1.0],
                     },
                 ]
